3929fe8587f93c4415dcf7f65c7570ee9206bf65
Muhtasim Mushfiq <muhtasim01@gmail.com>
2024-08-29 19:48:39 -0400

    Corrected typo

--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
   const [messages, setMessages] = useState([
     {
     role: 'assistant',
-    content: "Hi I'm the Headstarter Support Agent, how can I assit you today?"
+    content: "Hi I'm the Headstarter Support Agent, how can I assist you today?"
     },
   ])
 
